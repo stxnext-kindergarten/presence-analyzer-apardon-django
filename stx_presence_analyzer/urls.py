1b13846d5e0139b9597b400a9fd84552afa67f85
artpard <artur.pardon@stxnext.pl>
2014-06-02 09:45:38 +0200

    Fixed errors pointed in review

--- stx_presence_analyzer/urls.py
+++ stx_presence_analyzer/urls.py
@@ -10,13 +10,13 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', views.MainPage.as_view()),
+    url(r'^(?P<template>\w+)/$', views.MainPage.as_view()),
     url(r'^api/presence_weekday/(?P<user_id>\d+)/$',
         views.APIPresenceWeekday.as_view(), name='presence_weekday_api'),
     url(r'^api/mean_time_presence/(?P<user_id>\d+)/$',
         views.APIMeanTimePresence.as_view(), name='mean_time_presence_api'),
     url(r'^api/presence_start_end/(?P<user_id>\d+)/$',
         views.APIPresenceStartEnd.as_view(), name='presence_start_end_api'),
-    url(r'^users/$', views.Users.as_view(), name='users'),
-    url(r'^$', views.MainPage.as_view()),
-    url(r'^(?P<template>\w+)/$', views.MainPage.as_view()),
+    url(r'^api/users/$', views.Users.as_view(), name='users'),
 )
